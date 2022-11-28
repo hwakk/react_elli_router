1476447a808102596d04aa6b2d8d9612497ff864
hwakk <x_xhwakk@naver.com>
2022-11-28 15:02:03 +0900

    feat: React router 개발

---
+++ src/pages/Videos.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Videos(props) {
+  const [text, setText] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setText("");
+    navigate(`/videos/${text}`);
+  };
+
+  return (
+    <div>
+      Videos
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="video id:"
+          value={text}
+          onChange={handleChange}
+        />
+      </form>
+    </div>
+  );
+}
+
+export default Videos;
